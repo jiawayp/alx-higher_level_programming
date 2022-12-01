78e260beefba0bd2fa09d9a771384d6f3c11eb2c
jiawayp <pauljiaway4@gmail.com>
2022-12-01 14:59:11 -0800

    Task 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 import sys
 
-def av(): 
-    av = sys.argv 
-    l_av = len(av) - 1
-    if l_av > 1: 
-        print(l_av, 'arguments: ') 
-        for i in range(1, l_av + 1):
-            print(f'{i:d}: {av[i]}')
-    elif l_av == 1: 
-        print(l_av, 'argument: ')
-        for i in range(1, l_av + 1):
-            print(f'{i:d}: {av[i]}')
-    elif l_av == 0:
-        print(l_av, 'arguments.')
-av()
+if __name__ == '__main__':
+    av = sys.argv
+    count_av = len(av) - 1
+    if count_av > 1:
+        print(count_av, 'arguments: ')
+        for i in range(1, count_av + 1):
+            print('{:d}: {}'.format(i, av[i]))
+    elif count_av == 1:
+        print(count_av, 'argument: ')
+        for i in range(1, count_av + 1):
+            print(f'{i:d}: {av[i]}'.format(i, av[i]))
+    elif count_av == 0:
+        print(count_av, 'arguments.')
