e77c3794711e3e849bc0b1b350c7518b9b32dcab
jiawayp <pauljiaway4@gmail.com>
2022-12-01 14:08:19 -0800

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
 if __name__ == "__main__": 
-    """ 
-    Printing the Sum between two numbers
-    """ 
+    """Printing the Sum between two numbers""" 
     a = 1
     b = 2 
     print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
