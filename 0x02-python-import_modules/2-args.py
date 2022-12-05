4859cf08fd10c13a6abb57026368d46ea9aa4611
jiawayp <pauljiaway4@gmail.com>
2022-12-05 11:39:50 -0800

    Tasj 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,14 +4,14 @@ import sys
 """
 if __name__ == '__main__':
     av = sys.argv
-    count_av = len(av) - 1
-    if count_av > 1:
-        print(count_av, 'arguments: ')
-        for i in range(1, count_av + 1):
+    c_av = len(av) - 1
+    if c_av > 1:
+        print(c_av, 'arguments: ')
+        for i in range(1, c_av + 1):
             print('{:d}: {}'.format(i, av[i]))
-    elif count_av == 1:
-        print(count_av, 'argument: ')
-        for i in range(1, count_av + 1):
+    elif c_av == 1:
+        print(c_av, 'argument: ')
+        for i in range(1, c_av + 1):
             print('{:d}: {}'.format(i, av[i]))
-    elif count_av == 0:
-        print(count_av, 'arguments.')
+    elif c_av == 0:
+        print(c_av, 'arguments.')
