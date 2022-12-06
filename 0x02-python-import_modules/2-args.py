cff1dee4f69cf4b19ab58e2889d5a32348b52b26
jiawayp <pauljiaway4@gmail.com>
2022-12-06 01:49:56 -0800

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,11 +6,11 @@ if __name__ == '__main__':
     av = sys.argv
     c_av = len(av) - 1
     if c_av > 1:
-        print(c_av, 'arguments: ')
+        print(c_av, 'arguments:')
         for i in range(1, c_av + 1):
             print('{:d}: {}'.format(i, av[i]))
     elif c_av == 1:
-        print(c_av, 'argument: ')
+        print(c_av, 'argument:')
         for i in range(1, c_av + 1):
             print('{:d}: {}'.format(i, av[i]))
     elif c_av == 0:
