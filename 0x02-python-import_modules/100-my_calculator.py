d193b8c2d71961a8ea9a894be7d26c0b85858527
jiawayp <pauljiaway4@gmail.com>
2022-12-07 17:09:30 -0800

    Adance Task

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    import sys
+    from calculator_1 import add, mul, sub, div
+    av = sys.argv[1:]
+    c_argv = len(av)
+    operators = ['+', '-', '*', '/']
+    if c_argv != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif sys.argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] == '+':
+            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+        elif sys.argv[2] == '-':
+            print('{:d} - {:d} = {:d}'.format(a, b, sub(a,b)))
+        elif sys.argv[2] == '*':
+            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+        elif sys.argv[2] == '/':
+            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
