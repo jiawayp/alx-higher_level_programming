fb78395fe2817d318a206fc2a4d0e5c4023e8d32
jiawayp <pauljiaway4@gmail.com>
2022-12-07 17:17:48 -0800

    Adance Task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         if sys.argv[2] == '+':
             print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
         elif sys.argv[2] == '-':
-            print('{:d} - {:d} = {:d}'.format(a, b, sub(a,b)))
+            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
         elif sys.argv[2] == '*':
             print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
         elif sys.argv[2] == '/':
