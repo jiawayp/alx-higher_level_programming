0766ec0f146c804ceb728dc77184615b67650bad
jiawayp <pauljiaway4@gmail.com>
2022-12-09 11:23:53 -0800

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     score = max(a_dictionary.values(), default=None)
     for key, value in a_dictionary.items():
