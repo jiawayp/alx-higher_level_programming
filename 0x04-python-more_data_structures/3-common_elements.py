b90c5ddaa2459c06d045e8b5ddd81c270344ed36
jiawayp <pauljiaway4@gmail.com>
2022-12-09 09:30:04 -0800

    Task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    a_set = set(set_1)
-    b_set = set(set_2)
-
-    if (a_set & b_set):
-        return a_set & b_set
+    return set(set_1) & (set_2)
 
