316bec0f430412b4d0948f4533869b161a175d75
Paul Jiaway <pauljiawaay4@gmail.com>
2023-11-28 18:11:29 +0800

    Task 2 -ALX

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ if number < 0:
     digit = -digit
 output = "Last digit of {} is {} and is ".format(number, digit)
 if digit > 5:
-    out += "greater than 5"
+    output += "greater than 5"
 elif digit == 0:
     output += "0"
 else:
