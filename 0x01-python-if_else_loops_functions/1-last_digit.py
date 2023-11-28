a73a8eb0362d1708f210f24b66a969f25c5a793a
Paul Jiaway <pauljiawaay4@gmail.com>
2023-11-28 17:48:56 +0800

    Task 2 - ALX

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random 
+
+number = random.randint(-10000, 10000)
+digit = abs(number) % 10
+if number < 0:
+    digit = -digit
+output = "Last digit of {} is {} and is ".format(number, digit)
+if digit > 5:
+    output += "greater than 5"
+elif digit == 0:
+    output += "0"
+else:
+    output += "less than 6 and not 0"
+print(output)
