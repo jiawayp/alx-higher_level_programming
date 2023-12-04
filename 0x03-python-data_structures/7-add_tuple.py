83fb4329dbd19acbe9b00e492b406c4992923ac3
Paul Jiaway <pauljiawaay4@gmail.com>
2023-12-04 23:27:47 +0800

    Task 7-ALX

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,14 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     """Add two tuples."""
-    a = tuple_a[:2] + (0, 0)[:2 - len(tuple_a)]
-    b = tuple_b[:2] + (0, 0)[:2 - len(tuple_b)]
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
 
-    return (a[0] + b[0], a[1] + b[1])
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
