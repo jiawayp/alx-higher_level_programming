4f219463dd3fdb820c563895eddfa9f487715d4d
Paul Jiaway <pauljiawaay4@gmail.com>
2023-12-04 16:25:21 +0800

    Task 7-ALX

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,6 @@
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Extract the first two elements from tuple_a and tuple_b
-    a = tuple_a[:2]
-    b = tuple_b[:2]
+    """Add two tuples."""
+    a = tuple_a[:2] + (0, 0)[:2 - len(tuple_a)]
+    b = tuple_b[:2] + (0, 0)[:2 - len(tuple_b)]
 
-    # Pad the tuples with 0 if they have less than 2 elements
-    a += (0,) * (2 - len(a))
-    b += (0,) * (2 - len(b))
-
-    # Perform the element-wise addition
-    result = (a[0] + b[0], a[1] + b[1])
-
-    return result
+    return (a[0] + b[0], a[1] + b[1])
